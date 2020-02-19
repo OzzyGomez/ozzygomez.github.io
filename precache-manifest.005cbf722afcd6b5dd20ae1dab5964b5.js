ef5a0f1198af5829c114697483b193cbf9f85f98
Osvaldo villagomez <osvaldovillagomez@Osvaldos-MacBook-Pro.local>
2020-02-18 22:23:42 -0600

    Updates

---
+++ precache-manifest.005cbf722afcd6b5dd20ae1dab5964b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2bfc381e6620005faff8f51ecb18676",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4c920a931f32e500705",
+    "url": "/static/css/main.7d0da31c.chunk.css"
+  },
+  {
+    "revision": "0cf601510f8954bd47db",
+    "url": "/static/js/2.802f50f5.chunk.js"
+  },
+  {
+    "revision": "f4c920a931f32e500705",
+    "url": "/static/js/main.401c8aa5.chunk.js"
+  },
+  {
+    "revision": "c3ac564fcf8da7c39cd7",
+    "url": "/static/js/runtime-main.19f60ec2.js"
+  },
+  {
+    "revision": "3ca9ca61d9d2d90811e3ce6f71a251fa",
+    "url": "/static/media/Resume2020.3ca9ca61.pdf"
+  },
+  {
+    "revision": "4060fef86e85eae8316442f4f2dc3439",
+    "url": "/static/media/background.4060fef8.png"
+  },
+  {
+    "revision": "f0d86085e2a4ac67c913e18c06e647c1",
+    "url": "/static/media/illini.f0d86085.mp4"
+  },
+  {
+    "revision": "3c0c3b9a628461f656e222074fdc0b92",
+    "url": "/static/media/kelp.3c0c3b9a.mp4"
+  },
+  {
+    "revision": "7bdbc007bd4630eb49a8583d37f1b7ab",
+    "url": "/static/media/light.7bdbc007.mp4"
+  },
+  {
+    "revision": "a5c63d641194f63e52f2e7fa750578d6",
+    "url": "/static/media/lowbattery.a5c63d64.mp4"
+  }
+]);
